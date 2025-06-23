2a8eab80ce0132f52e84faa5c159fd5c9af71cad
jkfmCZ <martin06.jelinek@gmail.com>
2025-06-23 21:08:47 +0200

    s

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 from django.shortcuts import redirect
 from django.contrib.auth import views as auth_views
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', lambda request: redirect('main', permanent=False)),
@@ -9,7 +11,7 @@ urlpatterns = [
     path('contacts', views.contacts, name='contacts'),
     path('t', views.tester, name='tester'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', csrf_exempt(LogoutView.as_view()), name='logout'),
     path('conf/', views.conf, name='configuration'),
     path('msg', views.msg, name='msg'),
     path('register/', views.register_view, name='register'),
